b5373068b4fa37af8844b85625b7d1ffdc7263f9
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-05-27 15:27:52 -0300

    A relação entre os iterators e as abstrações do framework de Collections do Java

--- src/br/com/treinaweb/java/collections/Main.java
+++ src/br/com/treinaweb/java/collections/Main.java
@@ -4,17 +4,20 @@ import br.com.treinaweb.java.collections.models.Pessoa;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        List<Pessoa> pessoas = new LinkedList<>();
         pessoas.add(new Pessoa(1, "Treinaweb 1"));
         pessoas.add(new Pessoa(2, "Treinaweb 2"));
-//        for (int i = 0; i < pessoas.size(); i++) {
-//            Pessoa p = pessoas.get(i);
-//            System.out.println(p);
-//        }
+        System.out.println("Com FOR:");
+        for (int i = 0; i < pessoas.size(); i++) {
+            Pessoa p = pessoas.get(i);
+            System.out.println(p);
+        }
+        System.out.println("Com ITERATOR");
         Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
         while (iteratorPessoa.hasNext()) {
             Pessoa p = iteratorPessoa.next();
