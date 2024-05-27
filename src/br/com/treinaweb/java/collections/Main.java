99ac62fb9b6304f7ee552ea4c55333c9f783ab08
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-05-27 15:46:49 -0300

    Iterators e a exceção ConcurrentModificationException

--- src/br/com/treinaweb/java/collections/Main.java
+++ src/br/com/treinaweb/java/collections/Main.java
@@ -12,23 +12,29 @@ public class Main {
         List<Pessoa> pessoas = new LinkedList<>();
         pessoas.add(new Pessoa(1, "Treinaweb 1"));
         pessoas.add(new Pessoa(2, "Treinaweb 2"));
+        pessoas.add(new Pessoa(3, "Treinaweb 3"));
 
-        System.out.println("Com FOR:");
-        for (int i = 0; i < pessoas.size(); i++) {
-            Pessoa p = pessoas.get(i);
-            System.out.println(p);
-        }
+//        System.out.println("Com FOR:");
+//        for (int i = 0; i < pessoas.size(); i++) {
+//            Pessoa p = pessoas.get(i);
+//            System.out.println(p);
+//        }
+
         System.out.println("Com ITERATOR");
         Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
         while (iteratorPessoa.hasNext()) {
             Pessoa p = iteratorPessoa.next();
+            if (p.getId() == 1) {
+                iteratorPessoa.remove();
+            }
             System.out.println(p);
         }
+        System.out.println(pessoas);
 
-        System.out.println("Com FOR-EACH");
-        for (Pessoa p : pessoas) {
-            System.out.println(p);
-        }
+//        System.out.println("Com FOR-EACH");
+//        for (Pessoa p : pessoas) {
+//            System.out.println(p);
+//        }
 
         System.out.println("FIM!");
     }
