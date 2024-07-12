c65351d10d9935bb454ce41aa2e281087683e406
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-07-12 12:29:07 -0300

    Conhecendo o TreeSet

--- src/br/com/treinaweb/java/collections/MainSet.java
+++ src/br/com/treinaweb/java/collections/MainSet.java
@@ -2,23 +2,25 @@ package br.com.treinaweb.java.collections;
 
 import br.com.treinaweb.java.collections.models.Pessoa;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MainSet {
 
     public static void main(String[] args) {
-        Set<Pessoa> set = new LinkedHashSet<Pessoa>();
-        set.add(new Pessoa(1, "Treinaweb 1"));
+        NavigableSet<Pessoa> set = new TreeSet<Pessoa>();
         set.add(new Pessoa(2, "Treinaweb 2"));
-        set.add(null);
-        System.out.println(set);
-        set.add(new Pessoa(2, "Treinaweb 2"));
-        System.out.println(set);
-        for (Pessoa p : set) {
-            System.out.println(p);
-        }
+        set.add(new Pessoa(3, "Treinaweb 3"));
+        set.add(new Pessoa(4, "Treinaweb 4"));
+        set.add(new Pessoa(1, "Abel"));
+        Pessoa teste = set.higher(new Pessoa(2, "Treinaweb 2"));
+        System.out.println(teste);
+        //set.add(null);
+//        System.out.println(set);
+//        set.add(new Pessoa(2, "Treinaweb 2"));
+//        System.out.println(set);
+//        for (Pessoa p : set) {
+//            System.out.println(p);
+//        }
     }
 
 }
